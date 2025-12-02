753ba3963c9c6a30974d2d1a31b842f624b58a22
kimdogyun <sydg1214@gmail.com>
2025-12-02 16:05:19 +0900

    import

--- git_syntasx/1.env_settings.sh
+++ git_syntasx/1.env_settings.sh
@@ -25,5 +25,17 @@ git config --list
 # 2-1).git 폴더를 생성
 git init
 #2-2)원격지 주소를 추가
-git remote add origin 원격지주소g
+git remote add origin 원격지주소
+
+#다른 repo에서 clone을 받아 나의 repo로 올리기
+#1.커밋이력 그대로 가져가기
+git clone 레포주소
+git remote set-url origin 레포주소 #원격지 주소 변경
+
+#2.커밋이력 없이 가져가기
+#.git 폴더 삭제
+git init
+git remote add origin 원격지주소
+git checkout -b main #main브랜치 생성 및 변경 
+#git add , git commit , git push 작업 실행 
 
