91e026c4afdf2d701edc30cfe10d2f5478efc395
kimdogyun <sydg1214@gmail.com>
2025-12-02 17:09:37 +0900

    git

--- git_syntasx/2.basic_command.sh
+++ git_syntasx/2.basic_command.sh
@@ -4,5 +4,24 @@ git status
 # . 은 모든 수정/추가 사항 add
 git add . 
 
-# 특정파일만 add할 경우(경로까지 포함)
+# 특정파일만 add할 경우(경로까지 포함)!
 git add test_folder/test1.txt
+
+# commit을 통해 메시지타이틀과 메시지 내용을 커밋이력으로 생성
+git commit -m "메시지제목" -m "메시지내용"
+# git commit만 입력하고 엔터시 vi모드 -> 첫줄:타이틀,두번째준부터 내용 -> : wq 저장후 종료시 커밋ID생성
+git commit
+
+#commit 이력 확인
+git log     
+git log --oneline
+
+# head하단의 로그만 보이든것이 아니라, 모든 commi이력을조회
+# head : 현재 사용(checkout)하고 있는 commitID를 의미
+git log --all
+
+# 원격저장소로 업로드
+git push origin 브랜치명 
+#충동발생기 충동무시하고,로컬기준의 코드를 원경에 덮어쓰기 (절대사용금지)
+git oush origin 브랜치면 --force
+
